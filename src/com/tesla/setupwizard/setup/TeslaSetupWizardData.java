201ea1b332003a4157520624f1fb110b7d9e79d5
emancebo <emancebo@cyngn.com>
2015-07-21 10:07:43 -0700

    SetupWizard: check if mobile data is enabled from telephony API
    System property ro.com.android.mobiledata is no longer used
    
    issue-id: JALEBI-135
    Change-Id: Iac088d50dc50227e805b58b537c38082b6797291

--- src/com/tesla/setupwizard/setup/TeslaSetupWizardData.java
+++ src/com/tesla/setupwizard/setup/TeslaSetupWizardData.java
@@ -36,11 +36,11 @@ public class TeslaSetupWizardData extends AbstractSetupData {
 
     private boolean mTimeSet = false;
     private boolean mTimeZoneSet = false;
-    private boolean mMobileDataEnabled = SystemProperties
-            .getBoolean("ro.com.android.mobiledata", true);
+    private boolean mMobileDataEnabled;
 
     public TeslaSetupWizardData(Context context) {
         super(context);
+        mMobileDataEnabled = SetupWizardUtils.isMobileDataEnabled(context);
     }
 
     @Override
