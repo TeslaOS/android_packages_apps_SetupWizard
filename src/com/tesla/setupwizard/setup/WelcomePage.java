ef984e1c7a508de02b2ce0593feac10d086810d2
cretin45 <cretin45@gmail.com>
2015-10-27 12:44:11 -0700

    SetupWizard: Fix locale picker styling
    Change-Id: If336a8321b09513ec8b22f8138a47fb4d94e5913
    Signed-off-by: NoelMacwan <yulespencer@gmail.com>
    
    Conflicts:
    	src/com/tesla/setupwizard/ui/LocalePicker.java

--- src/com/tesla/setupwizard/setup/WelcomePage.java
+++ src/com/tesla/setupwizard/setup/WelcomePage.java
@@ -100,6 +100,7 @@ public class WelcomePage extends SetupPage {
         private final Runnable mUpdateLocale = new Runnable() {
             public void run() {
                 if (mCurrentLocale != null) {
+                    mLanguagePicker.setEnabled(false);
                     com.android.internal.app.LocalePicker.updateLocale(mCurrentLocale);
                 }
             }
@@ -150,6 +151,7 @@ public class WelcomePage extends SetupPage {
         }
 
         private void onLocaleChanged(Locale paramLocale) {
+            mLanguagePicker.setEnabled(true);
             Resources localResources = getActivity().getResources();
             Configuration localConfiguration1 = localResources.getConfiguration();
             Configuration localConfiguration2 = new Configuration();
